c7e18c3998a0ada528c190492bf6629f9375672d
FERNANDO GALDINO DA SILVA FILHO <T01POR0117@unimedfortaleza.com.br>
2019-01-23 11:55:47 -0300

    alterando app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  static PASS_WS='auditorvirtual:auditorvirtual2018ws';
+  static PASS_WS='****';
   static API_PESQUISAR_SERVICOS_COD="http://localhost:8081/auditorvirtual/servicos";
   static API_PESQUISAR_REGRAS_GERAL="http://localhost:8081/auditorvirtual/regrasgeral";
   static API_PESQUISAR_SALVAR_ALTERAR_REGRAS="http://localhost:8081/auditorvirtual/regras";
